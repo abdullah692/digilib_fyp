ebce72cfa546a9112b5ef1e710acbab9d74bc711
abdullah692 <abdullahsidd@gmail.com>
2022-03-19 03:20:47 +0500

    userProfile Updata

--- digilib-front-master/Components/User_Profile/UserProfile.js
+++ digilib-front-master/Components/User_Profile/UserProfile.js
@@ -1,4 +1,4 @@
-import { View, Text,StyleSheet, ScrollView,Image,TouchableOpacity } from 'react-native'
+import { View, Text, StyleSheet, ScrollView, Image, TouchableOpacity } from 'react-native'
 import React, { useState } from 'react'
 import UserData from './UserData.json'
 import Icon from 'react-native-vector-icons/FontAwesome'
@@ -9,11 +9,50 @@ import RNPrint from 'react-native-print';
 export default function UserProfile() {
   const [userdata, setuserData] = useState(UserData);
   console.log(userdata[1]);
-  
-  
-  const printPDF=async()=> {
+
+  let printData = []
+  userdata.slice(0, 1).map(data => {
+    let html = `
+    <span>Batch: ${data.Batch}</span> &nbsp &nbsp <span>Roll No: ${data.roll_no}</span>
+    <p>Year: ${data.year}</p>
+    <p>Name : ${data.name} </p>
+    <p>Department: ${data.Department}</p>
+    <p>Membership: ${data.Membership}</p>
+    <p>Cell-No: ${data.Cell_No}</p>
+    <p>Email: ${data.Email}</p>
+    `
+    printData.push(html)
+  })
+
+  const generateHTML = (userdata) => {
+    return `
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div style="font-size: 25px; text-align: center; font-family:calibri;">
+    <h2 style="margin-bottom: 50px;">User Profile</h2>
+    ${userdata.join(``)}
+    </div>
+   
+</body>
+</html>
+        
+      
+    `
+  }
+
+
+
+
+  const printPDF = async () => {
     const results = await RNHTMLtoPDF.convert({
-      html: '<h1>Custom converted PDF Document</h1>',
+      html: generateHTML(printData),
       fileName: 'test',
       base64: true,
     })
@@ -23,36 +62,36 @@ export default function UserProfile() {
 
   return (
     <View style={styles.container}>
-       <View>
-          <Text style={styles.heading}>User Profile</Text>
-            </View>
-            <TouchableOpacity>
-              <Icon name="file-pdf-o" size={30} style={styles.pdf} onPress={()=>printPDF()  }/>
-              </TouchableOpacity>
+      <View>
+        <Text style={styles.heading}>User Profile</Text>
+      </View>
+      <TouchableOpacity>
+        <Icon name="file-pdf-o" size={30} style={styles.pdf} onPress={() => printPDF()} />
+      </TouchableOpacity>
 
-            <Image style={{ width:300, height:170 ,marginVertical:15 }} source={
-                        require('../../assets/person.png')} resizeMode='contain' />
+      <Image style={{ width: 300, height: 170, marginVertical: 15 }} source={
+        require('../../assets/person.png')} resizeMode='contain' />
       <ScrollView>
         <View style={styles.border}>
-          {userdata.slice(0,1).map((item) => (
+          {userdata.slice(0, 1).map((item) => (
             <View>
-            <Text style={styles.info}>Batch: {item.Batch}{'\t\t\t\t'} Roll No: {item.roll_no} {'\n'}</Text>
-            <Text style={styles.info}>Year: {item.year}{'\n'}</Text>
-            <Text style={styles.info}>Name: {item.name}{'\n'}</Text>
-            <Text style={styles.info}>Department: {item.Department}
-            {'\n'}{'\n'}
-            Membership: {item.Membership}
-            {'\n'}{'\n'}
-            Cell-No: {item.Cell_No}
-            {'\n'}{'\n'}
-            Email : {item.Email}
-            </Text>
+              <Text style={styles.info}>Batch: {item.Batch}{'\t\t\t\t'} Roll No: {item.roll_no} {'\n'}</Text>
+              <Text style={styles.info}>Year: {item.year}{'\n'}</Text>
+              <Text style={styles.info}>Name: {item.name}{'\n'}</Text>
+              <Text style={styles.info}>Department: {item.Department}
+                {'\n'}{'\n'}
+                Membership: {item.Membership}
+                {'\n'}{'\n'}
+                Cell-No: {item.Cell_No}
+                {'\n'}{'\n'}
+                Email : {item.Email}
+              </Text>
             </View>
           ))}
         </View>
       </ScrollView>
-      </View>
-    
+    </View>
+
 
 
   )
@@ -68,31 +107,31 @@ const styles = StyleSheet.create({
     alignItems: 'center'
   },
   heading:
-    {
-        fontSize:45,
-       textAlign:'center',
-        borderColor:'#36B5E4',
-        color:'#000',
-        borderBottomWidth:3,
-        marginHorizontal:70,
-        marginVertical:20
+  {
+    fontSize: 45,
+    textAlign: 'center',
+    borderColor: '#36B5E4',
+    color: '#000',
+    borderBottomWidth: 3,
+    marginHorizontal: 70,
+    marginVertical: 20
 
-    },
-    border:
-    {
-      marginVertical:20,
-      marginHorizontal:10,
-      padding:10
-    },
-    info:
-    {
-      fontSize:20,
-      fontFamily:'sans-serif',
-      color:'black'
-    },
-    pdf:
-    {
-      marginTop:20,
-      marginLeft:'60%'
-    }
- })
+  },
+  border:
+  {
+    marginVertical: 20,
+    marginHorizontal: 10,
+    padding: 10
+  },
+  info:
+  {
+    fontSize: 20,
+    fontFamily: 'sans-serif',
+    color: 'black'
+  },
+  pdf:
+  {
+    marginTop: 20,
+    marginLeft: '60%'
+  }
+})
